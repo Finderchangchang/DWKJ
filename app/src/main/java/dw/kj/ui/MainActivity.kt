909de10d0728a55796a57d9e632fc22ba829a032
1031066280@qq.com <chang19930228>
2017-05-31 18:01:42 +0800

    Initial commit

---
+++ app/src/main/java/dw/kj/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package dw.kj.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import dw.kj.R
+
+import dw.kj.R.id.set_ip_ib
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/dw/kj/ui/SetIpActivity.kt
@@ -0,0 +1,14 @@
+package dw.kj.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import dw.kj.R
+
+class SetIpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_ip)
+    }
+}
